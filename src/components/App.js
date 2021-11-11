02693add29d7f3a07d4382acde878118c13672bc
shankaraxis <79733453+shankaraxis@users.noreply.github.com>
2021-11-11 09:12:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -147,12 +147,12 @@ function App() {
 			<nav className="navbar fixed-top mx-3">
 				<a
 					className="navbar-brand col-sm-3 col-md-2 mr-0 mx-4"
-					href="http://www.dappuniversity.com/bootcamp"
+					href="http://www.google.com"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
 					<img src={logo} className="App-logo" alt="logo" />
-					Dapp University
+					Mint Testing
 				</a>
 
 				{account ? (
